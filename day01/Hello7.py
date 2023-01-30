89b4112c999285e97fe0673441957518cdc461c5
MB-teacher <vincentjava@yahoo.co.tw>
2023-01-30 11:55:43 +0800

    python

--- day01/Hello7.py
+++ day01/Hello7.py
@@ -3,7 +3,10 @@
 # 求圓面積 = ? (印出的內容到小數點一位)
 import math
 
-r = 9
+r = input('請輸入半徑: ')
+print(r, type(r))
+r = int(r)  # 將 r 轉為 int 型態
+print(r, type(r))
 #pi = 3.1415236
 pi = math.pi  # 透過 python 內建的數學函式來取得圓周率
 print(pi)
