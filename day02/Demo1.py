76e12291027d649d9285e216930b357c89dfff21
MB-teacher <vincentjava@yahoo.co.tw>
2023-01-31 09:32:34 +0800

    python

---
+++ day02/Demo1.py
@@ -0,0 +1,18 @@
+# python 保留字
+import keyword
+print(keyword.kwlist)
+
+# 變數的覆值
+a = b = c = 100
+age, name = 18, "John"
+
+# 浮點數
+pi = 3.14
+value = 3.14e2  # 科學記號 e2 -> 表示10的2次方
+print(pi, value)
+print(type(pi), type(value))
+
+# 綜合練習
+a, b, c, d = 10, 20.5, True, 'Mary'
+print(a, b, c, d)
+print(type(a), type(b), type(c), type(d))
