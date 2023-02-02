0735818d7fc4e23317f3802ade4951ecf5493214
MB-teacher <vincentjava@yahoo.co.tw>
2023-02-02 09:18:36 +0800

    python

---
+++ day04/迴圈For.py
@@ -0,0 +1,16 @@
+import random as r
+# for loop 步進迴圈
+for i in range(1, 5):  # 1, 2, 3, 4
+    print(i)
+# -----------------------------------
+for i in range(1, 5):
+    print('java')
+# -----------------------------------
+for i in range(1, 5):
+    print(i, 'java')
+# -----------------------------------
+# 電腦選4個號碼
+for i in range(0, 4):
+    print(r.randint(1, 9), end=' ')
+print()
+
