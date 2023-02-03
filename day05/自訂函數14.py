12a75a9cfad360fbc9791c27a5faee3bad3fdcd7
MB-teacher <vincentjava@yahoo.co.tw>
2023-02-03 11:49:13 +0800

    python

---
+++ day05/自訂函數14.py
@@ -0,0 +1,8 @@
+emp1 = {'name': 'John', 'salary': 50000, 'program': ['Java', 'Python']}
+emp2 = {'name': 'Mary', 'salary': 70000, 'program': ['C++', 'Java']}
+emp3 = {'name': 'Alen', 'salary': 80000, 'program': ['VB', 'Python']}
+emp4 = {'name': 'Cube', 'salary': 60000, 'program': ['C#']}
+employees = [emp1, emp2, emp3, emp4]
+print(employees)
+# 請印出會 Python 的員工姓名 ?
+
