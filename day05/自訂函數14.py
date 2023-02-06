a62abf0c44da3cbb1ec64361f876652b64f1fb3f
MB-teacher <vincentjava@yahoo.co.tw>
2023-02-06 11:43:39 +0800

    python

--- day05/自訂函數14.py
+++ day05/自訂函數14.py
@@ -5,4 +5,11 @@ emp4 = {'name': 'Cube', 'salary': 60000, 'program': ['C#']}
 employees = [emp1, emp2, emp3, emp4]
 print(employees)
 # 請印出會 Python 的員工姓名 ?
+names = []
+for emp in employees:
+    # print(emp['program'], emp['program'].count('Python'))
+    if emp['program'].count('Python') > 0:
+        names.append(emp['name'])
+
+print(names)
 
