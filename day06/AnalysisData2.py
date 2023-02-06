349acf17f2a11ab6144fabe86c20d93395603f29
MB-teacher <vincentjava@yahoo.co.tw>
2023-02-06 10:37:11 +0800

    python

--- day06/AnalysisData2.py
+++ day06/AnalysisData2.py
@@ -1,3 +1,4 @@
+import statistics as stat
 from day06.ReadFile import get_employees_from_file
 if __name__ == '__main__':
     employees_salary = get_employees_from_file('salary.txt')
@@ -9,5 +10,12 @@ if __name__ == '__main__':
     # CV 越大表示分散程度越大(集中度小)反之CV 越小表示分散程度越小(集中度大)
     # CV(變異係數) 與 SD(標準差)的不同是
     # CV 用來比較不同單位的差異, SD 用來比較同單位的差異
-
+    # 計算 salary 的 CV
+    salary = [emp['salary'] for emp in employees_salary]
+    salary_cv = stat.stdev(salary) / stat.mean(salary)
+    # 計算 age 的 CV
+    age = [emp['age'] for emp in employees_age]
+    age_cv = stat.stdev(age) / stat.mean(age)
+    print('salary cv: {:.3f} age cv: {:.3f}'.format(salary_cv, age_cv))
+    print('{0}的分散程度大'.format('salary' if salary_cv > age_cv else 'age'))
 
