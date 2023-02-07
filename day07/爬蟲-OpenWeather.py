507f0ceee5cf96d66e9efab56ba23ba7ced4cd8c
MB-teacher <vincentjava@yahoo.co.tw>
2023-02-07 11:33:50 +0800

    python

--- day07/爬蟲-OpenWeather.py
+++ day07/爬蟲-OpenWeather.py
@@ -1,2 +1,9 @@
 # 天氣資料查詢
 # 網路位置: https://api.openweathermap.org/data/2.5/weather?q={},tw&appid={}
+import json
+import requests
+
+city_name = 'taoyuan'
+key = 'fcc57465b76d35357c84e4e30fe2431a'
+url = 'https://api.openweathermap.org/data/2.5/weather?q={},tw&appid={}'.format(city_name, key)
+print(url)
