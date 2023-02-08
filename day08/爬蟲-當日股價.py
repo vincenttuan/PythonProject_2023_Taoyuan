b10f00b944f668db604f993809ae2a3104ca1665
MB-teacher <vincentjava@yahoo.co.tw>
2023-02-08 11:56:02 +0800

    python

--- day08/爬蟲-當日股價.py
+++ day08/爬蟲-當日股價.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 url = 'https://tw.stock.yahoo.com/quote/2330'
 resp = requests.get(url)
 soup = BeautifulSoup(resp.text, 'html.parser')
-tag = '<span class="Fz(32px) Fw(b) Lh(1) Mend(16px) D(f) Ai(c) C($c-trend-up)">'
-for span in soup.findAll('span'):
-    if tag in str(span):
-        print(span.text)
+# <span class="Fz(32px) Fw(b) Lh(1) Mend(16px) D(f) Ai(c) C($c-trend-up)">538</span>
+class_value = 'Fz(32px) Fw(b) Lh(1) Mend(16px) D(f) Ai(c) C($c-trend-up)'
+span = soup.find(name="span", attrs={"class": class_value})
+print(span.text)
