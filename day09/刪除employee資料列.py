729740bba6d1a1da8e8e064c6239876fd86d3c47
MB-teacher <vincentjava@yahoo.co.tw>
2023-02-09 11:47:11 +0800

    python

--- day09/刪除employee資料列.py
+++ day09/刪除employee資料列.py
@@ -1,11 +1,12 @@
 import sqlite3
 # 刪除 employee 資料列
 sql = 'delete from employee where id = ?'
-conn = sqlite3.connect(sql)
+conn = sqlite3.connect('demo.db')
 cursor = conn.cursor()
 delete_id = 2  # 要刪除的 id
 param = [delete_id]
 cursor = cursor.execute(sql, param)
 print('刪除筆數: {}'.format(cursor.rowcount))
-
+conn.commit()
+conn.close()
 
