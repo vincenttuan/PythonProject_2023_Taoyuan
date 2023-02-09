245acbf977b01e937da18188f82b0a198f384b1b
MB-teacher <vincentjava@yahoo.co.tw>
2023-02-09 11:25:24 +0800

    python

--- day09/查詢employee資料表.py
+++ day09/查詢employee資料表.py
@@ -21,23 +21,24 @@ def find_one(id):
     return row
 
 
+def print_employee(employees):
+    # 將資料印出
+    print('%2s %-6s %8s %-24s' % ('ID', 'Name', 'Salary', 'Time'))
+    print('---------------------------------------')
+    if employees is None or employees[0] is None:
+        print(' 查無資料')
+    else:
+        for emp in employees:
+            print('%2d %-6s %8s %-24s' % (emp[0], emp[1], '{:,}'.format(emp[2]), emp[3]))
+    print('---------------------------------------')
+
+
 if __name__ == '__main__':
     # 多筆
     employees = find_all()
     # 將資料印出
-    print('%2s %-6s %8s %-24s' % ('ID', 'Name', 'Salary', 'Time'))
-    print('---------------------------------------')
-    for emp in employees:
-        print('%2d %-6s %8s %-24s' % (emp[0], emp[1], '{:,}'.format(emp[2]), emp[3]))
-    print('---------------------------------------')
+    print_employee(employees)
 
     # 單筆
     emp = find_one(1)
-    # 將資料印出
-    print('%2s %-6s %8s %-24s' % ('ID', 'Name', 'Salary', 'Time'))
-    print('---------------------------------------')
-    if emp is not None:
-        print('%2d %-6s %8s %-24s' % (emp[0], emp[1], '{:,}'.format(emp[2]), emp[3]))
-    else:
-        print(' 查無資料 ')
-    print('---------------------------------------')
+    print_employee([emp])
