1d837906366856758b2f84ae100e47904090e88d
MB-teacher <vincentjava@yahoo.co.tw>
2023-02-10 10:17:43 +0800

    python

--- day10/detection/偵測照片上的人臉.py
+++ day10/detection/偵測照片上的人臉.py
@@ -18,5 +18,18 @@ print(face_cascade)
 frame = cv2.imread('../sample_image/test.jpg')
 print(frame)
 
+# 將彩色圖片(RGB)進行灰階(Gray)處理資加效率
+gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+print(gray)
+
+# 偵測臉部, 得到臉部區域座標與長寬(x, y, w, h)
+faces = face_cascade.detectMultiScale(
+    gray,  # 待偵測圖片
+    scaleFactor=1.1,   # 檢測粒度(數字越小越精準(但速度慢), 反之數字越大越模糊(速度快))
+    minNeighbors=5,  # 檢測次數(每個目標至少要檢測通過幾次才算成功，才被認定是 face)
+    minSize=(30, 30),  # 搜尋比對最小尺寸
+    flags=cv2.CASCADE_SCALE_IMAGE  # 比對類型
+)
+print('臉部座標(x, y, w, h):', faces)
 
 
