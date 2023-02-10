f2135960318101bcc31a831e2a06e06d9f778345
MB-teacher <vincentjava@yahoo.co.tw>
2023-02-10 11:30:23 +0800

    python

--- day10/detection/偵測WebCam.py
+++ day10/detection/偵測WebCam.py
@@ -13,6 +13,21 @@ cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 while True:
     ret, frame = cap.read()
     print(ret, frame)
+    # 灰階
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # 偵測臉部, 得到臉部區域座標與長寬(x, y, w, h)
+    faces = face_cascade.detectMultiScale(
+        gray,  # 待偵測圖片
+        scaleFactor=1.1,  # 檢測粒度(數字越小越精準(但速度慢), 反之數字越大越模糊(速度快))
+        minNeighbors=5,  # 檢測次數(每個目標至少要檢測通過幾次才算成功，才被認定是 face)
+        minSize=(30, 30),  # 搜尋比對最小尺寸
+        flags=cv2.CASCADE_SCALE_IMAGE  # 比對類型
+    )
+    # 在 face 周圍畫上矩形
+    for (x, y, w, h) in faces:
+        # 參數: frame, 左上角座標, 右下角座標, BGR色碼, 框線的寬度
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
     # 將 frame 顯示
     cv2.imshow('Detect face', frame)
 
