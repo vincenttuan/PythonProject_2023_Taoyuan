3fe6131865ecd9ea218e96ec05756ceade4bcaa7
MB-teacher <vincentjava@yahoo.co.tw>
2023-02-10 10:05:37 +0800

    python

--- day10/detection/偵測照片上的人臉.py
+++ day10/detection/偵測照片上的人臉.py
@@ -8,3 +8,10 @@ pip install pillow
 pip install opencv-python
 pip install opencv-contrib-python
 '''
+import cv2
+
+# 人臉特徵檔
+face_cascade = cv2.CascadeClassifier('../haarcascade/haarcascade_frontalface_default.xml')
+print(face_cascade)
+
+
