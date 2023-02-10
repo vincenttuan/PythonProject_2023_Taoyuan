260d8df827d09247242ec00568c0f98f67617729
MB-teacher <vincentjava@yahoo.co.tw>
2023-02-10 11:34:46 +0800

    python

--- day10/detection/偵測WebCam.py
+++ day10/detection/偵測WebCam.py
@@ -2,6 +2,9 @@ import cv2
 
 # 人臉特徵檔
 face_cascade = cv2.CascadeClassifier('../haarcascade/haarcascade_frontalface_default.xml')
+# 眼睛特徵檔
+# eye_cascade = cv2.CascadeClassifier('../haarcascade/haarcascade_eye.xml')  # 無戴眼鏡
+eye_cascade = cv2.CascadeClassifier('../haarcascade/haarcascade_eye_tree_eyeglasses.xml')  # 有戴眼鏡
 
 # 設定 Webcam
 cap = cv2.VideoCapture(0)
@@ -27,6 +30,21 @@ while True:
     for (x, y, w, h) in faces:
         # 參數: frame, 左上角座標, 右下角座標, BGR色碼, 框線的寬度
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        # 在 face 內進行眼部偵測
+        roi_color = frame[y:y + h, x:x + w]  # 人臉區域-彩色(y, x)
+        roi_gray = gray[y:y + h, x:x + w]  # 人臉區域-灰階(y, x)
+        # 眼睛偵測
+        eyes = eye_cascade.detectMultiScale(
+            roi_gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30),
+            flags=cv2.CASCADE_SCALE_IMAGE
+        )
+        # 眼睛繪製
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 0, 255), 2)
 
     # 將 frame 顯示
     cv2.imshow('Detect face', frame)
